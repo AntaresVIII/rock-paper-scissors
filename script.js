a0590162bffdd3db956ba0b1cc1d4c604dfa8c30
Antares <enteringvalhalla@gmail.com>
2021-07-04 21:25:53 +0100

    First usable version

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+// Generate random choice for the computer to play //
+
 const computerPlay = ["Rock", "Paper", "Scissors"];
 const random = Math.floor(Math.random() * computerPlay.length)
-console.log(random, computerPlay[random]);
+    // console.log(computerPlay[random]);
+
+let playerSelection = prompt("Choose!", "");
+let computerSelection = computerPlay[random];
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (computerSelection == playerSelection) {
+        return "Draw!";
+      } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")
+      ) { return "You lose!";
+        }
+        else {
+            return "You win!";
+        }
+}
 
 
-// function playRound(playerSelection, computerSelection) {
-
- // }
-
-// const computerSelection = computerPlay;
+console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection);
