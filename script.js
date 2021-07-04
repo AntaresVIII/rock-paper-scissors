65c367d0a51c7af507ecf00958b7299e42b9b8b1
Antares <enteringvalhalla@gmail.com>
2021-07-04 20:27:25 +0100

    Modified

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let computerPlay = ['Rock', 'Paper', 'Scissors'];
-aaaa
+const computerPlay = ["Rock", "Paper", "Scissors"];
+const random = Math.floor(Math.random() * computerPlay.length)
+console.log(random, computerPlay[random]);
+
+
+// function playRound(playerSelection, computerSelection) {
+
+ // }
+
+// const computerSelection = computerPlay;
